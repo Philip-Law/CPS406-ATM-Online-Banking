a15da0a6fd2e2032c673e85632dc065c72db3698
Philip Law <law.philip17@gmail.com>
2023-03-22 07:07:58 -0400

    Add test cases for Transaction class

--- test_Transaction.py
+++ test_Transaction.py
@@ -1,18 +1,18 @@
 from Transaction import Transaction
+import pytest
 
-class TestTransaction:
-    def test_get_id(self):
-        withdrawAction = Transaction("401", "03/22/2023", "withdraw", 500)
-        assert withdrawAction.get_id() == "401"
+@pytest.fixture
+def withdrawAction():
+    return Transaction("401", "03/22/2023", "Withdraw", 500)
 
-    def test_get_date(self):
-        withdrawAction = Transaction("401", "03/22/2023", "withdraw", 500)
-        assert withdrawAction.get_date() == "03/22/2023"
+def test_get_id(withdrawAction):
+    assert withdrawAction.get_id() == "401"
 
-    def test_get_action(self):
-        withdrawAction = Transaction("401", "03/22/2023", "withdraw", 500)
-        assert withdrawAction.get_action_type() == "withdraw"
-    
-    def test_get_amount(self):
-        withdrawAction = Transaction("401", "03/22/2023", "withdraw", 500)
-        assert withdrawAction.get_amount() == 500
+def test_get_date(withdrawAction):
+    assert withdrawAction.get_date() == "03/22/2023"
+
+def test_get_action(withdrawAction):
+    assert withdrawAction.get_action_type() == "Withdraw"
+
+def test_get_amount(withdrawAction):
+    assert withdrawAction.get_amount() == 500
