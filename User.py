80e729ffc96f02930661a6b2543257df3b0d9f65
Buukesh <78332928+Buukesh@users.noreply.github.com>
2023-03-27 09:54:16 -0400

    Update User.py

--- User.py
+++ User.py
@@ -2,13 +2,16 @@ from Account import Account
 
 class User:
     def __init__(self, name: str, chequing_balance: int, savings_balance: int):
+        # Intialize instance variables for User
         self.name = name
         self.accounts = [Account(chequing_balance), Account(savings_balance)]
 
     def get_name(self):
+        # Get name of user
         return self.name
 
     def get_account(self, account_type: str) -> Account:
+        # Get account dependent on given account type
         if account_type == 'Chequing':
             return self.accounts[0]
         elif account_type == 'Savings':
