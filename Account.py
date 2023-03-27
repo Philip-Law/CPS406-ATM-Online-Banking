13d8b28226a890c22c68124de67462edf1af5ea0
Buukesh <78332928+Buukesh@users.noreply.github.com>
2023-03-27 09:28:26 -0400

    Update Account.py

--- Account.py
+++ Account.py
@@ -3,26 +3,33 @@ from random import randint
 from datetime import date
 
 class Account:
+    # Starting transaction id
     transaction_id = 10000000
 
+    # Generate transaction id for every transaction made in account
     @classmethod
     def generate_transaction_id(cls):
         cls.transaction_id += 1
         return str(cls.transaction_id)
 
+    # Intialize balance and list of transactions
     def __init__(self, balance: int):
         self.balance = balance
         self.transaction_history = []
 
-    def deposit(self, amount):
+    # Deposit amount to account
+    def deposit(self, amount: int):
         self.balance += amount
         transaction = Transaction(Account.generate_transaction_id(), date.today(), "Deposit", amount)
+        # Raise error for invalid amount
         if self.balance <= 0:
             raise ValueError("Invalid Deposit")
         self.transaction_history.append(transaction)
         return transaction
 
-    def withdraw(self, amount):
+    # Withdraw amount to account
+    def withdraw(self, amount: int):
+        # Raise error for invalid amount
         if self.balance < amount:
             raise ValueError("Insufficient balance")
         self.balance -= amount
@@ -36,6 +43,5 @@ class Account:
     def get_transaction_history(self):
         return self.transaction_history
     
-    
-    def add_transaction(self, new_transaction: Transaction) -> None:
+    def add_transaction(self, new_transaction: Transaction):
         self.transaction_history.append(new_transaction)
